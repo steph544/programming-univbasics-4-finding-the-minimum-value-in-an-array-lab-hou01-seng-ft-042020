c02af731b2bff980732480e9ba5bb4128edd487e
Stephanie Cheney <steph@scheneyphotography.com>
2020-04-08 16:09:32 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -2,7 +2,7 @@ def find_min_value(array)
   value=19 
   counter=0 
   while counter<array.length do 
-    if array[counter]<19 
+    if array[counter]< value  
       value = array[counter]
     end 
   counter+=1 
