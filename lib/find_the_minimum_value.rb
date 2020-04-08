9bf1fc26862b6b963e0454db0704123740b33127
Stephanie Cheney <steph@scheneyphotography.com>
2020-04-08 16:08:11 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,12 +1,11 @@
 def find_min_value(array)
-  value=10 
+  value=11 
   counter=0 
   while counter<array.length do 
-    if value>10 
-      value= array[counter]
+    if value<10 
+      value = array[counter]
     end 
   counter+=1 
 end 
 value 
-  # Add your solution here
 end
